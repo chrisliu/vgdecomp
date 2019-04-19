52032fd17e240f3e06a78072c565ff4c1b91a5ae
Ryan <ryan.h.nishimoto@gmail.com>
2019-04-18 17:51:02 -0700

    removed camelcase

--- src/Graph.hpp
+++ src/Graph.hpp
@@ -12,14 +12,15 @@ class Graph{
     private:
         vector<NodeTraversal*> vertices;
         vector<Edge*> edges;
-        inline Node newNode(int64_t id);
-        inline void addEdge(int64_t upstream_id, int64_t downstream_id, bool isDirected);
+        inline Node new_node(int64_t id);
+        inline void add_edge(int64_t upstream_id, int64_t downstream_id, bool isDirected);
     
     public:
-        void addVertex(int64_t id);
-        void addDirectedEdge(int64_t upstream_id, int64_t downstream_id);
-        void addUndirectedEdge(int64_t id1, int64_t id2);
-        vector<Node*> connectedNodes(int64_t parentId);
+        void add_vertex(int64_t id);
+        void add_directed_edge(int64_t upstream_id, int64_t downstream_id);
+        void add_undirected_edge(int64_t id1, int64_t id2);
+        vector<Edge*> edges_on_start(int64_t id);
+        vector<Edge*> edges_on_end(int64_t id);
 
 
 };
