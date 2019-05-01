d4c7bb83bd65e097b07eae203f09e4066da6da4c
Ryan <ryan.h.nishimoto@gmail.com>
2019-05-01 11:39:42 -0700

    updated parameters for reachable_nodes and helper function

--- src/BidirectedGraph.cpp
+++ src/BidirectedGraph.cpp
@@ -1,5 +1,4 @@
 #include "BidirectedGraph.hpp"
-
 void BidirectedGraph::add_edge(uint64_t id1, uint64_t id2, bool from_left, bool to_right){
     BidirectedEdge* tempedge = new BidirectedEdge(id1, id2);
     tempedge->from_left = from_left;
@@ -9,5 +8,17 @@ void BidirectedGraph::add_edge(uint64_t id1, uint64_t id2, bool from_left, bool 
 }
 
 void BidirectedGraph::populate_reachable_nodes(){
+    unordered_map<uint64_t, vector<BidirectedEdge> >::iterator iter = edges.begin();
+    set<uint64_t> temp;
+    while(iter!=edges.end()){
+
+    }
     
+}
+
+void BidirectedGraph::reachable_nodes_helper(uint64_t id, set<uint64_t> set, bool into_left){
+
+}
+vector<uint64_t> BidirectedGraph::get_reachable_nodes(uint64_t id){
+    return reachable_nodes.at(id);
 }
--- src/BidirectedGraph.hpp
+++ src/BidirectedGraph.hpp
@@ -2,12 +2,14 @@
 #define BidirectedGraph_hpp
 #include "BidirectedEdge.hpp"
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <set>
 using namespace std;
 class BidirectedGraph {
     private:
-        map<uint64_t, vector<uint64_t> > reachable_nodes;
-        map<uint64_t, vector<BidirectedEdge> > edges;
+        unordered_map<uint64_t, vector<uint64_t> > reachable_nodes;
+        unordered_map<uint64_t, vector<BidirectedEdge> > edges;
+        void reachable_nodes_helper(uint64_t id, set<uint64_t> set, bool into_left);
     public:
         vector<uint64_t> get_reachable_nodes(uint64_t id);
         void add_edge(uint64_t id1, uint64_t id2, bool from_left, bool to_right);
