69df5997de76738836bcf77f437088a0e66ed1ea
Chris Liu <cliu158@ucsc.edu>
2019-05-09 11:19:47 -0700

    Updated bidirected edge and node definitions

--- src/BidirectedEdge.hpp
+++ src/BidirectedEdge.hpp
@@ -1,15 +1,17 @@
 #ifndef BidirectedEdge_hpp
 #define BidirectedEdge_hpp
-#include <cstdint>
+
+#include "Definitions.hpp"
+
 class BidirectedEdge {
     public:
-        uint64_t id1;
-        uint64_t id2;
-        bool from_left; 
-                        // If it’s exiting from the left side of id1
+        nid_t id1;
+        nid_t id2;
+        bool from_left;
         bool to_right;
-                        // If it’s entering from the right side of id1
-        BidirectedEdge(uint64_t id1, uint64_t id2);
+        BidirectedEdge(nid_t id1, nid_t id2, 
+            bool from_left = false, bool to_right = false);
         bool operator==(const BidirectedEdge& other);
 };
-#endif
+
+#endif /* BidirectedEdge_hpp */
--- src/BidirectedNode.hpp
+++ src/BidirectedNode.hpp
@@ -1,12 +1,13 @@
 #ifndef BidirectedNode_hpp
 #define BidirectedNode_hpp
-#include <stdint.h>
-// This class is not used?
-class BidirectedNode{
+
+#include "Definitions.hpp"
+
+class BidirectedNode {
     public:
-        uint64_t id;
-        BidirectedNode(int64_t id);
+        nid_t id;
+        BidirectedNode(nid_t id);
         bool operator==(const BidirectedNode& other);
 };
 
-#endif
+#endif /* BidirectedNode_hpp */
