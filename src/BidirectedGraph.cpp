83ecc31b5961185f5939229ea0ca4d44b54ef39c
Ryan <ryan.h.nishimoto@gmail.com>
2019-05-09 13:51:31 -0700

    testing build

--- src/BidirectedGraph.cpp
+++ src/BidirectedGraph.cpp
@@ -1,13 +1,13 @@
 #include "BidirectedGraph.hpp"
 
-private unordered_map<uint64_t, set<uint64_t> > sets;
+unordered_map<uint64_t, set<uint64_t> > sets;
 
 void BidirectedGraph::add_edge(uint64_t id1, uint64_t id2, bool from_left, bool to_right){
-    BidirectedEdge* tempedge = new BidirectedEdge(id1, id2);
-    tempedge->from_left = from_left;
-    tempedge->to_right = to_right;
-    edges.insert(make_pair(id1, tempedge));
-    edges.insert(make_pair(id2, tempedge));
+    BidirectedEdge tempedge(id1, id2);
+    tempedge.from_left = from_left;
+    tempedge.to_right = to_right;
+    edges.insert({id1, tempedge});
+    edges.insert({id2, tempedge});
 }
 
 void BidirectedGraph::populate_reachable_nodes(){
@@ -16,11 +16,11 @@ void BidirectedGraph::populate_reachable_nodes(){
         vector<BidirectedEdge>::iterator veciter = mapiter->second.begin();
         while(veciter!=mapiter->second.end()){
             if(veciter->id1==mapiter->first){
-                set.insert(veciter->id2);
+                sets.at(mapiter->first).insert(veciter->id2);
                 reachable_nodes_helper(veciter->id2, sets.at(mapiter->first), veciter->to_right);
             }
             else if(veciter->id2==mapiter->first){
-                set.insert(veciter->id1);
+                sets.at(mapiter->first).insert(veciter->id1);
                 reachable_nodes_helper(veciter->id1, sets.at(mapiter->first), veciter->from_left);
             }
             veciter++;
@@ -44,7 +44,7 @@ void BidirectedGraph::reachable_nodes_helper(uint64_t id, set<uint64_t> set, boo
         vector<BidirectedEdge>::iterator veciter = mapiter->second.begin();
         while(veciter!=mapiter->second.end()){
             if(veciter->id1==mapiter->first && veciter->from_left!=into_left){
-                set.insert(veciter->id2)
+                set.insert(veciter->id2);
                 reachable_nodes_helper(veciter->id2, set, !veciter->to_right);
             }
             else if(veciter->id2==mapiter->first && veciter->to_right==into_left){
@@ -60,6 +60,13 @@ vector<uint64_t> BidirectedGraph::get_reachable_nodes(uint64_t id){
     return reachable_nodes.at(id);
 }
 
-void print_reachable_nodes(){
-    //unordered_map<uint64_t, vector<uint64_t> >::iterator reachable = 
+void BidirectedGraph::print_reachable_nodes(){
+    unordered_map<uint64_t, vector<uint64_t> >::iterator reachable = reachable_nodes.begin();
+    while(reachable!=reachable_nodes.end()){
+        vector<uint64_t>::iterator nodeiter = reachable->second.begin();
+        printf("Printing path connected nodes from %d:\n", reachable->first);
+        while(nodeiter!=reachable->second.end()){
+            printf("%d\n", *nodeiter);
+        }
+    }
 }
--- src/BidirectedGraphTEST.cpp
+++ src/BidirectedGraphTEST.cpp
@@ -2,18 +2,18 @@
 
 int main(int argc, char const *argv[])
 {
-    BidirectedGraph graph1 = BidirectedGraph();
-    graph1.add_edge(1, 2, false, false);
+    BidirectedGraph graph1;
+    graph1.add_edge((uint64_t)1, (uint64_t)2, false, false);
     printf("created and added edges to graph1.\n");
-    BidirectedGraph graph2 = BidirectedGraph();
-    graph2.add_edge(1, 2, false, false);
-    graph2.add_edge(2, 3, false, false);
+    BidirectedGraph graph2;
+    graph2.add_edge((uint64_t)1, (uint64_t)2, false, false);
+    graph2.add_edge((uint64_t)2, (uint64_t)3, false, false);
     printf("created and added edges to graph2.\n");
-    BidirectedGraph graph3 = BidirectedGraph();
-    graph3.add_edge(1, 2, false, false);
-    graph3.add_edge(1, 3, false, false);
-    graph3.add_edge(2, 4, false, false);
-    graph3.add_edge(3, 4, false, false);
+    BidirectedGraph graph3;
+    graph3.add_edge((uint64_t)1, (uint64_t)2, false, false);
+    graph3.add_edge((uint64_t)1, (uint64_t)3, false, false);
+    graph3.add_edge((uint64_t)2, (uint64_t)4, false, false);
+    graph3.add_edge((uint64_t)3, (uint64_t)4, false, false);
     printf("created and added edges to graph3.\n");
     graph1.populate_reachable_nodes();
     printf("populated reachable nodes map in graph1.\n");
@@ -21,8 +21,12 @@ int main(int argc, char const *argv[])
     printf("populated reachable nodes map in graph2.\n");
     graph3.populate_reachable_nodes();
     printf("populated reachable nodes map in graph3.\n");
-    printf("printing reachable nodes in graph1:\n");
     graph1.print_reachable_nodes();
+    graph2.print_reachable_nodes();
+    graph3.print_reachable_nodes();
+
+
+    printf("finished tests.");
 
     return 0;
 }
