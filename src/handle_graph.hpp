cf689cbecd2bbb570417df5dab32d72e3ed6ac39
Chris Liu <cliu158@ucsc.edu>
2019-05-27 11:52:43 -0700

    Added basic handlegraph files

---
+++ src/handle_graph.hpp
@@ -0,0 +1,233 @@
+#ifndef HANDLEGRAPH_HANDLE_GRAPH_HPP_INCLUDED
+#define HANDLEGRAPH_HANDLE_GRAPH_HPP_INCLUDED
+
+/** \file 
+ * Defines the base HandleGraph interface.
+ */
+ 
+#include "handlegraph/types.hpp"
+#include "handlegraph/iteratee.hpp"
+
+#include <functional>
+#include <string>
+#include <iostream>
+
+namespace handlegraph {
+
+
+/**
+ * This is the interface that a graph that uses handles needs to support.
+ * It is also the interface that users should code against.
+ */
+class HandleGraph {
+public:
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Interface that needs to be implemented
+    ////////////////////////////////////////////////////////////////////////////
+   
+    /// Method to check if a node exists by ID
+    virtual bool has_node(nid_t node_id) const = 0;
+   
+    /// Look up the handle for the node with the given ID in the given orientation
+    virtual handle_t get_handle(const nid_t& node_id, bool is_reverse = false) const = 0;
+    
+    /// Get the ID from a handle
+    virtual nid_t get_id(const handle_t& handle) const = 0;
+    
+    /// Get the orientation of a handle
+    virtual bool get_is_reverse(const handle_t& handle) const = 0;
+    
+    /// Invert the orientation of a handle (potentially without getting its ID)
+    virtual handle_t flip(const handle_t& handle) const = 0;
+    
+    /// Get the length of a node
+    virtual size_t get_length(const handle_t& handle) const = 0;
+    
+    /// Get the sequence of a node, presented in the handle's local forward
+    /// orientation.
+    virtual std::string get_sequence(const handle_t& handle) const = 0;
+    
+    /// Return the number of nodes in the graph
+    virtual size_t get_node_count() const = 0;
+    
+    /// Return the smallest ID in the graph, or some smaller number if the
+    /// smallest ID is unavailable. Return value is unspecified if the graph is empty.
+    virtual nid_t min_node_id() const = 0;
+    
+    /// Return the largest ID in the graph, or some larger number if the
+    /// largest ID is unavailable. Return value is unspecified if the graph is empty.
+    virtual nid_t max_node_id() const = 0;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Stock interface that uses backing virtual methods
+    ////////////////////////////////////////////////////////////////////////////
+    
+    // We can't actually overload on the bool-vs-void-returning lambdas we want
+    // to accomodate even in C++14. See <https://stackoverflow.com/a/6194623>;
+    // basically the std::function constructor for void-returning functions can
+    // implicitly take other functions and destroy their returned values. So we
+    // need to use templates still. We template on the individual unique types
+    // of literal lambdas people pass us.
+    
+    /// Loop over all the handles to next/previous (right/left) nodes. Passes
+    /// them to a callback. If called with a bool-returning invocable thing,
+    /// can stop early when the function returns false. Returns true if we
+    /// finished and false if we stopped early.
+    template<typename Iteratee>
+    bool follow_edges(const handle_t& handle, bool go_left, const Iteratee& iteratee) const;
+    
+    /// Loop over all the nodes in the graph in their local forward
+    /// orientations, in their internal stored order. If called with a
+    /// bool-returning invocable thing, can stop early when the function
+    /// returns false. Returns true if we finished and false if we stopped
+    /// early. Can be told to run in parallel, in which case stopping after a
+    /// false return value is on a best-effort basis and iteration order is not
+    /// defined.
+    template<typename Iteratee>
+    bool for_each_handle(const Iteratee& iteratee, bool parallel = false) const;
+    
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Additional optional interface with a default implementation
+    ////////////////////////////////////////////////////////////////////////////
+        
+    /// Get the number of edges on the right (go_left = false) or left (go_left
+    /// = true) side of the given handle. The default implementation is O(n) in
+    /// the number of edges returned, but graph implementations that track this
+    /// information more efficiently can override this method.
+    virtual size_t get_degree(const handle_t& handle, bool go_left) const;
+    
+    /// Returns true if there is an edge that allows traversal from the left
+    /// handle to the right handle. By default O(n) in the number of edges
+    /// on left, but can be overridden with more efficient implementations.
+    virtual bool has_edge(const handle_t& left, const handle_t& right) const;
+    
+    /// Convenient wrapper of has_edge for edge_t argument.
+    inline bool has_edge(const edge_t& edge) {
+        return has_edge(edge.first, edge.second);
+    }
+    
+    /// Returns one base of a handle's sequence, in the orientation of the
+    /// handle.
+    virtual char get_base(const handle_t& handle, size_t index) const;
+    
+    /// Returns a substring of a handle's sequence, in the orientation of the
+    /// handle. If the indicated substring would extend beyond the end of the
+    /// handle's sequence, the return value is truncated to the sequence's end.
+    /// By default O(n) in the size of the handle's sequence, but can be overriden.
+    virtual std::string get_subsequence(const handle_t& handle, size_t index, size_t size) const;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Concrete utility methods
+    ////////////////////////////////////////////////////////////////////////////
+    
+    /// Get the locally forward version of a handle
+    handle_t forward(const handle_t& handle) const;
+    
+    /// A pair of handles can be used as an edge. When so used, the handles have a
+    /// canonical order and orientation.
+    edge_t edge_handle(const handle_t& left, const handle_t& right) const;
+    
+    /// Such a pair can be viewed from either inward end handle and produce the
+    /// outward handle you would arrive at.
+    handle_t traverse_edge_handle(const edge_t& edge, const handle_t& left) const;
+    
+    /// Loop over all edges in their canonical orientation (as returned by
+    /// edge_handle) as edge_t items and execute an iteratee on each one. If
+    /// the iteratee returns bool, and it returns false, stop iteration. Return
+    /// true if the iteration completed and false if it stopped early. If run
+    /// in parallel (parallel = true), stopping early is best-effort.
+    template<typename Iteratee>
+    bool for_each_edge(const Iteratee& iteratee, bool parallel = false) const;
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Backing protected virtual methods that need to be implemented
+    ////////////////////////////////////////////////////////////////////////////
+    
+protected:
+    
+    /// Loop over all the handles to next/previous (right/left) nodes. Passes
+    /// them to a callback which returns false to stop iterating and true to
+    /// continue. Returns true if we finished and false if we stopped early.
+    virtual bool follow_edges_impl(const handle_t& handle, bool go_left, const std::function<bool(const handle_t&)>& iteratee) const = 0;
+    
+    /// Loop over all the nodes in the graph in their local forward
+    /// orientations, in their internal stored order. Stop if the iteratee
+    /// returns false. Can be told to run in parallel, in which case stopping
+    /// after a false return value is on a best-effort basis and iteration
+    /// order is not defined. Returns true if we finished and false if we 
+    /// stopped early.
+    virtual bool for_each_handle_impl(const std::function<bool(const handle_t&)>& iteratee, bool parallel = false) const = 0;
+    
+    
+};
+
+/*
+ * Defines an interface for serialization and deserialization for handle graph,
+ * which can be co-inherited alongside HandleGraph.
+ */
+class SerializableHandleGraph {
+    
+public:
+    
+    /// Write the contents of this graph to an ostream.
+    virtual void serialize(std::ostream& out) const = 0;
+    
+    /// Sets the contents of this graph to the contents of a serialized graph from
+    /// an istream. The serialized graph must be from the same implementation of the
+    /// HandleGraph interface as is calling deserialize(). Can only be called by an
+    /// empty graph.
+    virtual void deserialize(std::istream& in) = 0;
+};
+
+
+////////////////////////////////////////////////////////////////////////////
+// Template Implementations
+////////////////////////////////////////////////////////////////////////////
+
+template<typename Iteratee>
+bool HandleGraph::follow_edges(const handle_t& handle, bool go_left, const Iteratee& iteratee) const {
+    return follow_edges_impl(handle, go_left, BoolReturningWrapper<Iteratee, handle_t>::wrap(iteratee));
+}
+
+template<typename Iteratee>
+bool HandleGraph::for_each_handle(const Iteratee& iteratee, bool parallel) const {
+    return for_each_handle_impl(BoolReturningWrapper<Iteratee, handle_t>::wrap(iteratee), parallel);
+}
+
+template<typename Iteratee>
+bool HandleGraph::for_each_edge(const Iteratee& iteratee, bool parallel) const {
+    auto wrapped_iteratee = BoolReturningWrapper<Iteratee, edge_t>::wrap(iteratee);
+    // (If we pre-cast our lambda to std::function we won't generate a new
+    // template instantiation for it each time we are instantiated.)
+    return for_each_handle((std::function<bool(const handle_t&)>)[&](const handle_t& handle) -> bool {
+        bool keep_going = true;
+        // Filter to edges where this node is lower ID or any rightward
+        // self-loops. 
+        follow_edges(handle, false, (std::function<bool(const handle_t&)>)[&](const handle_t& next) -> bool {
+            if (get_id(handle) <= get_id(next)) {
+                keep_going = wrapped_iteratee(edge_handle(handle, next));
+            }
+            return keep_going;
+        });
+        if (keep_going) {
+            // Filter to edges where this node is lower ID or leftward
+            // reversing self-loop.
+            follow_edges(handle, true, (std::function<bool(const handle_t&)>)[&](const handle_t& prev) -> bool {
+                if (get_id(handle) < get_id(prev) ||
+                    (get_id(handle) == get_id(prev) && get_is_reverse(prev))) {
+                    keep_going = wrapped_iteratee(edge_handle(prev, handle));
+                }
+                return keep_going;
+            });
+        }
+        
+        return keep_going;
+    }, parallel);
+}
+
+
+}
+
+#endif
---
+++ src/types.hpp
@@ -0,0 +1,87 @@
+#ifndef HANDLEGRAPH_TYPES_HPP_INCLUDED
+#define HANDLEGRAPH_TYPES_HPP_INCLUDED
+
+/** \file
+ * Defines the types used for handles, and user-facing operators to work with them.
+ */
+
+#include <cstdint>
+#include <utility>
+#include <functional>
+
+namespace handlegraph {
+
+/// Represents an id
+typedef int64_t nid_t;
+    
+[[deprecated("id_t collides with a standard type, use nid_t instead")]]
+typedef nid_t id_t;
+    
+/// Represents an offset
+typedef std::size_t off_t;
+    
+/// Represents a position
+typedef std::tuple<nid_t, bool, off_t> pos_t;
+    
+/// Represents the internal id of a node traversal
+struct handle_t { char data[sizeof(nid_t)]; };
+    
+/// Represents an edge in terms of its endpoints
+typedef std::pair<handle_t, handle_t> edge_t;
+    
+/// Represents the internal id of a path entity
+struct path_handle_t { char data[sizeof(int64_t)]; };
+    
+/// A step handle is an opaque reference to a single step of an oriented node on a path in a graph
+struct step_handle_t { char data[2 * sizeof(int64_t)]; };
+
+/// Define equality on handles
+bool operator==(const handle_t& a, const handle_t& b);
+
+/// Define inequality on handles
+bool operator!=(const handle_t& a, const handle_t& b);
+
+/// Define equality on path handles
+bool operator==(const path_handle_t& a, const path_handle_t& b);
+
+/// Define inequality on path handles
+bool operator!=(const path_handle_t& a, const path_handle_t& b);
+
+/// Define equality on step handles
+bool operator==(const step_handle_t& a, const step_handle_t& b);
+
+/// Define inequality on step handles
+bool operator!=(const step_handle_t& a, const step_handle_t& b);
+
+}
+
+// Hashes need to be in the std namespace
+namespace std {
+
+/**
+ * Define hashes for handles.
+ */
+template<> struct hash<handlegraph::handle_t> {
+public:
+    inline size_t operator()(const handlegraph::handle_t& handle) const {
+        // TODO: We can't include util.cpp for as_integer because it includes us!
+        // But we need anyone with access to the handle_t to be able to hash it.
+        // So we just convert to integer manually.
+        return std::hash<int64_t>()(reinterpret_cast<const uint64_t&>(handle));
+    }
+};
+
+/**
+ * Define hashes for path handles.
+ */
+template<> struct hash<handlegraph::path_handle_t> {
+public:
+    inline size_t operator()(const handlegraph::path_handle_t& path_handle) const {
+        return std::hash<int64_t>()(reinterpret_cast<const uint64_t&>(path_handle));
+    }
+};
+
+}
+
+
+#endif
