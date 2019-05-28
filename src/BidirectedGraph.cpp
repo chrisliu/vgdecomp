54735418aa796513b9e0bce4d5a09a2729391507
Chris Liu <cliu158@ucsc.edu>
2019-05-27 18:11:49 -0700

    Implemented node related operations for handlegraph

--- src/BidirectedGraph.cpp
+++ src/BidirectedGraph.cpp
@@ -1,12 +1,13 @@
 #include "BidirectedGraph.hpp"
 
-unordered_map<uint64_t, set<uint64_t> > sets;
+unordered_map<nid_t, set<nid_t> > sets;
 
-vector<uint64_t> BidirectedGraph::get_reachable_nodes(uint64_t id){
+
+vector<nid_t> BidirectedGraph::get_reachable_nodes(nid_t id){
     return reachable_nodes.at(id);
 }
 
-void BidirectedGraph::add_edge(uint64_t id1, uint64_t id2, bool from_left, bool to_right){
+void BidirectedGraph::add_edge(nid_t id1, nid_t id2, bool from_left, bool to_right){
     BidirectedEdge tempedge(id1, id2);
     tempedge.from_left = from_left;
     tempedge.to_right = to_right;
@@ -22,19 +23,19 @@ bool BidirectedGraph::is_acyclic(){
 }
 
 void BidirectedGraph::populate_reachable_nodes(){
-    unordered_map<uint64_t, vector<BidirectedEdge> >::iterator mapiter = edges.begin();
+    unordered_map<nid_t, vector<BidirectedEdge> >::iterator mapiter = edges.begin();
     while(mapiter!=edges.end()){
         vector<BidirectedEdge>::iterator veciter = mapiter->second.begin();
         while(veciter!=mapiter->second.end()){
 
             if(veciter->id1==mapiter->first){
-                sets.emplace(make_pair(mapiter->first, set<uint64_t>()));
+                sets.emplace(make_pair(mapiter->first, set<nid_t>()));
                 sets[mapiter->first].insert(veciter->id2);
 
                 reachable_nodes_helper(veciter->id2, sets.at(mapiter->first), !veciter->to_right);
             }
             else if(veciter->id2==mapiter->first){
-                sets.emplace(make_pair(mapiter->first, set<uint64_t>()));
+                sets.emplace(make_pair(mapiter->first, set<nid_t>()));
                 sets[mapiter->first].insert(veciter->id1);
                 reachable_nodes_helper(veciter->id1, sets.at(mapiter->first), veciter->from_left);
             }
@@ -42,10 +43,10 @@ void BidirectedGraph::populate_reachable_nodes(){
         }
         mapiter++;
     }
-    unordered_map<uint64_t, set<uint64_t> >:: iterator setmapiter = sets.begin();
+    unordered_map<nid_t, set<nid_t> >:: iterator setmapiter = sets.begin();
     while(setmapiter!=sets.end()){
-        set<uint64_t>:: iterator setiter = sets.at(setmapiter->first).begin();
-        reachable_nodes.emplace(setmapiter->first, vector<uint64_t>());
+        set<nid_t>:: iterator setiter = sets.at(setmapiter->first).begin();
+        reachable_nodes.emplace(setmapiter->first, vector<nid_t>());
         while(setiter!=sets.at(setmapiter->first).end()){
             reachable_nodes[setmapiter->first].push_back(*setiter);
             setiter++;
@@ -54,7 +55,7 @@ void BidirectedGraph::populate_reachable_nodes(){
     }
 }
 
-void BidirectedGraph::reachable_nodes_helper(uint64_t id, set<uint64_t>& set, bool into_left){
+void BidirectedGraph::reachable_nodes_helper(nid_t id, set<nid_t>& set, bool into_left){
         
         vector<BidirectedEdge>::iterator veciter = edges.at(id).begin();
         while(veciter!=edges.at(id).end()){
@@ -73,9 +74,9 @@ void BidirectedGraph::reachable_nodes_helper(uint64_t id, set<uint64_t>& set, bo
 
 
 void BidirectedGraph::print_reachable_nodes(){
-    unordered_map<uint64_t, vector<uint64_t> >::iterator reachable = reachable_nodes.begin();
+    unordered_map<nid_t, vector<nid_t> >::iterator reachable = reachable_nodes.begin();
     while(reachable!=reachable_nodes.end()){
-        vector<uint64_t>::iterator nodeiter = reachable->second.begin();
+        vector<nid_t>::iterator nodeiter = reachable->second.begin();
         printf("Printing path connected nodes from %llu:\n", reachable->first);
         while(nodeiter!=reachable->second.end()){
             printf("%llu\n", *nodeiter);
@@ -83,4 +84,79 @@ void BidirectedGraph::print_reachable_nodes(){
         }
         reachable++;
     }
+}
+
+/// Method to check if a node exists by ID
+bool BidirectedGraph::has_node(nid_t nodeid) const {
+    return edges.find(nodeid) != edges.end();
+}
+
+/// Look up the handle for the node with the given ID in the given orientation
+handle_t BidirectedGraph::get_handle(const nid_t& node_id, bool is_reverse) const {
+    return encoder.pack(node_id, is_reverse);
+}
+
+/// Get the ID from a handle
+nid_t BidirectedGraph::get_id(const handle_t& handle) const {
+    return encoder.unpack_number(handle);
+}
+
+/// Get the orientation of a handle
+bool BidirectedGraph::get_is_reverse(const handle_t& handle) const {
+    return encoder.unpack_bit(handle);
+}
+
+/// Invert the orientation of a handle (potentially without getting its ID)
+handle_t BidirectedGraph::flip(const handle_t& handle) const {
+    return encoder.toggle_bit(handle);
+}
+
+/// Get the length of a node
+size_t BidirectedGraph::get_length(const handle_t& handle) const {
+    return 0;
+}
+
+/// Get the sequence of a node, presented in the handle's local forward
+/// orientation.
+std::string BidirectedGraph::get_sequence(const handle_t& handle) const {
+    return "";
+}
+
+/// Return the number of nodes in the graph
+size_t BidirectedGraph::get_node_count() const {
+    return edges.size();
+}
+
+/// Return the smallest ID in the graph, or some smaller number if the
+/// smallest ID is unavailable. Return value is unspecified if the graph is empty.
+nid_t BidirectedGraph::min_node_id() const {
+    edge_map::const_iterator res = min_element(edges.begin(), edges.end());
+    // Should work with default comparator, otherwise use
+    // edge_map::const_iterator res = min_element(edges.begin(), edges.end(),
+    //                                     [](const nid_t& id1, const nid_t& id2) {
+    //                                         return id1 < id2;
+    //                                     })
+    if (res != edges.end()) {
+        return res->first;
+    }
+    return 0;
+}
+
+/// Return the largest ID in the graph, or some larger number if the
+/// largest ID is unavailable. Return value is unspecified if the graph is empty.
+nid_t BidirectedGraph::max_node_id() const {
+    edge_map::const_iterator res = max_element(edges.begin(), edges.end());
+    if (res != edges.end()) {
+        return res->first;
+    }
+    return 0;
+}
+
+bool BidirectedGraph::follow_edges_impl(const handle_t& handle, bool go_left, const std::function<bool(const handle_t&)>& iteratee) const {
+
+}
+        
+
+bool BidirectedGraph::for_each_handle_impl(const std::function<bool(const handle_t&)>& iteratee, bool parallel = false) const {
+
 }
--- src/BidirectedGraph.hpp
+++ src/BidirectedGraph.hpp
@@ -15,64 +15,68 @@
 using namespace std;
 using namespace handlegraph;
 
+typedef unordered_map<nid_t, vector<nid_t>> node_map; 
+typedef unordered_map<nid_t, vector<BidirectedEdge>> edge_map;
+
 class BidirectedGraph : public HandleGraph {
     private:
-        unordered_map<uint64_t, vector<uint64_t> > reachable_nodes;
-        unordered_map<uint64_t, vector<BidirectedEdge> > edges;
-        void reachable_nodes_helper(uint64_t id, set<uint64_t>& set, bool into_left);
+        const number_bool_packing encoder;
+        node_map reachable_nodes;
+        edge_map edges;
+        void reachable_nodes_helper(nid_t id, set<nid_t>& set, bool into_left);
     public:
-        vector<uint64_t> get_reachable_nodes(uint64_t id);
-        void add_edge(uint64_t id1, uint64_t id2, bool from_left, bool to_right);
+        vector<nid_t> get_reachable_nodes(nid_t id);
+        void add_edge(nid_t id1, nid_t id2, bool from_left, bool to_right);
         bool is_acyclic();
         void populate_reachable_nodes();
         void print_reachable_nodes();
 
-    /// Method to check if a node exists by ID
-    bool has_node(nid_t node_id) const;
-   
-    /// Look up the handle for the node with the given ID in the given orientation
-    handle_t get_handle(const nid_t& node_id, bool is_reverse = false) const;
+        /// Method to check if a node exists by ID
+        bool has_node(nid_t node_id) const;
     
-    /// Get the ID from a handle
-    nid_t get_id(const handle_t& handle) const;
-    
-    /// Get the orientation of a handle
-    bool get_is_reverse(const handle_t& handle) const;
-    
-    /// Invert the orientation of a handle (potentially without getting its ID)
-    handle_t flip(const handle_t& handle) const;
-    
-    /// Get the length of a node
-    size_t get_length(const handle_t& handle) const;
-    
-    /// Get the sequence of a node, presented in the handle's local forward
-    /// orientation.
-    std::string get_sequence(const handle_t& handle) const;
-    
-    /// Return the number of nodes in the graph
-    size_t get_node_count() const;
-    
-    /// Return the smallest ID in the graph, or some smaller number if the
-    /// smallest ID is unavailable. Return value is unspecified if the graph is empty.
-    nid_t min_node_id() const;
-    
-    /// Return the largest ID in the graph, or some larger number if the
-    /// largest ID is unavailable. Return value is unspecified if the graph is empty.
-    nid_t max_node_id() const;    
-    
-protected:
-    
-    /// Loop over all the handles to next/previous (right/left) nodes. Passes
-    /// them to a callback which returns false to stop iterating and true to
-    /// continue. Returns true if we finished and false if we stopped early.
-    bool follow_edges_impl(const handle_t& handle, bool go_left, const std::function<bool(const handle_t&)>& iteratee) const = 0;
-    
-    /// Loop over all the nodes in the graph in their local forward
-    /// orientations, in their internal stored order. Stop if the iteratee
-    /// returns false. Can be told to run in parallel, in which case stopping
-    /// after a false return value is on a best-effort basis and iteration
-    /// order is not defined. Returns true if we finished and false if we 
-    /// stopped early.
-    bool for_each_handle_impl(const std::function<bool(const handle_t&)>& iteratee, bool parallel = false) const;
+        /// Look up the handle for the node with the given ID in the given orientation
+        handle_t get_handle(const nid_t& node_id, bool is_reverse = false) const;
+        
+        /// Get the ID from a handle
+        nid_t get_id(const handle_t& handle) const;
+        
+        /// Get the orientation of a handle
+        bool get_is_reverse(const handle_t& handle) const;
+        
+        /// Invert the orientation of a handle (potentially without getting its ID)
+        handle_t flip(const handle_t& handle) const;
+        
+        /// Get the length of a node
+        size_t get_length(const handle_t& handle) const;
+        
+        /// Get the sequence of a node, presented in the handle's local forward
+        /// orientation.
+        std::string get_sequence(const handle_t& handle) const;
+        
+        /// Return the number of nodes in the graph
+        size_t get_node_count() const;
+        
+        /// Return the smallest ID in the graph, or some smaller number if the
+        /// smallest ID is unavailable. Return value is unspecified if the graph is empty.
+        nid_t min_node_id() const;
+        
+        /// Return the largest ID in the graph, or some larger number if the
+        /// largest ID is unavailable. Return value is unspecified if the graph is empty.
+        nid_t max_node_id() const;    
+        
+    protected:
+        
+        /// Loop over all the handles to next/previous (right/left) nodes. Passes
+        /// them to a callback which returns false to stop iterating and true to
+        /// continue. Returns true if we finished and false if we stopped early.
+        bool follow_edges_impl(const handle_t& handle, bool go_left, const std::function<bool(const handle_t&)>& iteratee) const;
+        
+        /// Loop over all the nodes in the graph in their local forward
+        /// orientations, in their internal stored order. Stop if the iteratee
+        /// returns false. Can be told to run in parallel, in which case stopping
+        /// after a false return value is on a best-effort basis and iteration
+        /// order is not defined. Returns true if we finished and false if we 
+        /// stopped early.
+        bool for_each_handle_impl(const std::function<bool(const handle_t&)>& iteratee, bool parallel = false) const;
 };
 #endif
