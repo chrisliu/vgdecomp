8147254c46bb116c01cb2043edd338d467740e4e
Chris Liu <cliu158@ucsc.edu>
2019-06-10 18:44:26 -0700

    Pushed old bundle finding algorithm

---
+++ src/algorithms/find_bundles.cpp
@@ -0,0 +1,111 @@
+#include "find_bundles.hpp"
+
+/// For each id a pair of bools are stored.
+/// The first bool is if the node has been traversed going left
+/// The second bool is if the node has been traversed going right
+typedef std::unordered_map<nid_t, std::pair<bool, bool>> traversed_t;
+
+std::vector<Bundle> find_bundles(const HandleGraph* g) {
+    std::vector<Bundle> bundles;
+    traversed_t traversed_nodes;
+
+    g->for_each_handle([&] (const handle_t& handle) {
+        if (is_unexplored(handle, traversed_nodes, g)) {
+            auto ret = is_in_bundle(handle, traversed_nodes, g);
+            if (ret.first) {
+                bundles.push_back(ret.second);
+            }
+        }
+
+        if (is_unexplored(handle, traversed_nodes, g, true)) {
+            auto ret = is_in_bundle(handle, traversed_nodes, g, true);
+            if (ret.first) {
+                bundles.push_back(ret.second);
+            }
+        }
+    });
+
+    return bundles;
+}
+
+std::pair<bool, Bundle> is_in_bundle(const handle_t& handle, traversed_t& traversed_nodes, const HandleGraph* g, bool go_left = false) {
+    Bundle bundle;
+    // Get which side of the bundle the handle is on
+    bool handle_dir     = g->get_is_reverse(handle);
+    bool is_handle_left = (handle_dir && go_left) || (!handle_dir && !go_left);
+
+    // Insert edges from opposite side
+    g->follow_edges(handle, !is_handle_left, [&](const handle_t& opp_handle) {
+        bundle.add_init_node(handle, is_handle_left);
+    });
+
+    // If there isn't anything, terminate
+    if (opposite_side.size() == 0) {
+        return std::make_pair(false, bundle);
+    }
+
+    // For each node that's on the opposite side of the original handle
+    // add it to an unordered set. (Excluded for the first node in the for loop)
+    // If a node that's added to the set didn't exist before, terminate
+    // the algorithm early and let the function caller know that this handle
+    // is not in a bundle
+    bool first = true;
+    for (const handle_t& opp_handle : opposite_side) {
+        bool not_bundle = g->follow_edges(opp_handle, is_handle_left, [&](const handle_t& same_handle) -> bool {
+            auto ret = handle_side.insert(same_handle);
+            if (!first && ret.second) {
+                return false;
+            }
+            return true;
+        });
+
+        if (not_bundle) {
+            return std::make_pair(false, bundle);
+        }
+
+        if (first) {
+            first = false;
+        }
+    }
+
+    // For each node that's on the opposite side of the nodes on the opposite 
+    // side of the original handle add it to an unordered set.
+    // If a node that's added to the set didn't exist before, terminate
+    // the algorithm early and let the function caller know that this handle
+    // is not in a bundle
+    for (const handle_t& same_handle : handle_side) {
+        bool not_bundle = g->follow_edges(same_handle, is_reverse, [&](const handle_t& opp_handle) -> bool {
+            auto ret = opposite_side.insert(opp_handle);
+            return ret.second;
+        });
+
+        if (not_bundle) {
+            return std::make_pair(false, bundle);
+        }
+    }
+
+    return std::make_pair(true, bundle);
+}
+
+inline bool is_unexplored(const handle_t& handle, traversed_t& traversed_nodes, const HandleGraph* g, bool is_reverse = false) {
+    nid_t id         = g->get_id(handle);
+    bool handle_left = g->get_is_reverse(handle);
+    bool is_right    = (handle_left && is_reverse) || (!handle_left && !is_reverse); 
+
+    traversed_nodes.emplace(id, std::pair<bool, bool>());
+    // Work on this... originally wanted to use the is_right bool to index
+    // either the first or second element of the pair
+    bool ret;
+    if (is_right) {
+        ret = traversed_nodes[id].second;
+        traversed_nodes[id].second = true;
+    } else {
+        ret = traversed_nodes[id].first;
+        traversed_nodes[id].first = true;
+    }
+    return ret;
+}
+
+inline void cache_node() {
+
+}
---
+++ src/algorithms/find_bundles.hpp
@@ -0,0 +1,15 @@
+#ifndef VG_ALGORITHMS_FIND_BUNDLE_HPP_INCLUDED
+#define VG_ALGORITHMS_FIND_BUNDLE_HPP_INCLUDED
+
+#include "../BidirectedGraph.hpp"
+#include "bundle.hpp"
+
+/// Locates all bundles in a given graph.
+/// Walks in both directions of a node to check. Nodes that have been
+/// walked in a particular direction will be cached. 
+/// Returns all bundles that have been found. The return format will
+/// be a vector of pairs of vectors containing handles. Each pair
+/// will denote the left side and the right side of a bundle.
+std::vector<Bundle> find_bundles(const HandleGraph* g);
+
+#endif /* VG_ALGORITHMS_FIND_BUNDLE_HPP_INCLUDED */
