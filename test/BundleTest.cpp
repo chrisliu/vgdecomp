4eb5ea3c0b2db191bf45d57d1f48aa576d39feb0
Chris Liu <chrisliu1999@gmail.com>
2019-10-12 17:23:56 -0700

    Updated test to print out better debug information

--- test/BundleTest.cpp
+++ test/BundleTest.cpp
@@ -2,8 +2,22 @@
 
 #include "../src/BidirectedGraph.hpp"
 #include "../src/algorithms/find_bundles.hpp"
+#include "../src/algorithms/bundle.hpp"
 
 using namespace std;
+
+void print_bundle(BidirectedGraph* g, Bundle& bundle) {
+    cout << "Left" << endl;
+    bundle.get_bundleside(true).traverse_bundle([&](const handle_t& l_handle) {
+        cout << g->get_id(l_handle) << endl;
+        return true;
+    });
+    cout << "Right" << endl;
+    bundle.get_bundleside(false).traverse_bundle([&](const handle_t& r_handle) {
+        cout << g->get_id(r_handle) << endl;
+        return true;
+    }); 
+}
 
 int main(int argc, char* argv[]) {
     int exit_code = EXIT_SUCCESS;
@@ -20,7 +34,11 @@ int main(int argc, char* argv[]) {
     auto bundles = find_bundles(&g);
 
     // Print information about the found bundle
-    cout << bundles.size() << endl;    
+    cout << "Bundle count: " << bundles.size() << endl;    
+    for (int i = 0; i < bundles.size(); i++) {
+        cout << "Bundle " << i + 1 << endl;
+        print_bundle(&g, *(bundles.begin() + i));
+    }
 
     return exit_code;
 }
