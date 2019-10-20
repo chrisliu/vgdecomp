d46cc3eff6905bdbd8d329b0e8b09af7645c0adb
Chris Liu <chrisliu1999@gmail.com>
2019-10-20 13:54:45 -0700

    Updated test printout to be more verbose.

--- test/BundleTest.cpp
+++ test/BundleTest.cpp
@@ -10,14 +10,16 @@ using namespace std;
 void print_bundle(BidirectedGraph* g, Bundle& bundle) {
     cout << "Left" << endl;
     bundle.get_bundleside(true).traverse_bundle([&](const handle_t& l_handle) {
-        cout << g->get_id(l_handle) << endl;
+        cout << g->get_id(l_handle) << ((g->get_is_reverse(l_handle)) ? '-' : '+') << endl;
         return true;
     });
     cout << "Right" << endl;
     bundle.get_bundleside(false).traverse_bundle([&](const handle_t& r_handle) {
-        cout << g->get_id(r_handle) << endl;
+        cout << g->get_id(r_handle) << ((g->get_is_reverse(r_handle)) ? '-' : '+') << endl;
         return true;
     }); 
+    cout << "Is a trivial bundle:  " << ((bundle.is_trivial()) ? "true" : "false") << endl;
+    cout << "Has reversed node(s): " << ((bundle.has_reversed_node()) ? "true" : "false") << endl;
 }
 
 /// Usage:
@@ -29,6 +31,15 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
         cout << i << "\t" << argv[i] << endl;
         BidirectedGraph g = BidirectedGraphBuilder::build_graph(argv[i]);
+        auto bundles = find_bundles(&g);
+        for (auto bundle : bundles) {
+            print_bundle(&g, bundle);
+        }
+        cout << "Nodes: ";
+        g.for_each_handle([&](const handle_t& handle) {
+            cout << g.get_id(handle) << " ";
+        });
+        cout << endl;
     }
 
     return exit_code;
