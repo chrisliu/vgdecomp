6305c3c10f201718e3ca0ae0d9417f3443301671
Chris Liu <chrisliu1999@gmail.com>
2019-10-20 13:11:37 -0700

    Changed so test graphs are loaded by BidirectedGraphBuilder from filenames supplied in args.

--- test/BundleTest.cpp
+++ test/BundleTest.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "../src/BidirectedGraph.hpp"
+#include "../src/BidirectedGraphBuilder.hpp"
 #include "../src/algorithms/find_bundles.hpp"
 #include "../src/algorithms/bundle.hpp"
 
@@ -19,25 +20,15 @@ void print_bundle(BidirectedGraph* g, Bundle& bundle) {
     }); 
 }
 
+/// Usage:
+/// Call BundleTest binary with the test graph json files as the arguments
+/// Ex: ./BundleTest test/bundle_test_graph/00_trivial.json
 int main(int argc, char* argv[]) {
     int exit_code = EXIT_SUCCESS;
 
-    // Basic graph
-    //    -- 2
-    // 1 -|
-    //    -- 3
-
-    BidirectedGraph g;
-    g.add_edge(1, 2, false, false);
-    g.add_edge(1, 3, false, false);
-
-    auto bundles = find_bundles(&g);
-
-    // Print information about the found bundle
-    cout << "Bundle count: " << bundles.size() << endl;    
-    for (int i = 0; i < bundles.size(); i++) {
-        cout << "Bundle " << i + 1 << endl;
-        print_bundle(&g, *(bundles.begin() + i));
+    for (int i = 1; i < argc; i++) {
+        cout << i << "\t" << argv[i] << endl;
+        BidirectedGraph g = BidirectedGraphBuilder::build_graph(argv[i]);
     }
 
     return exit_code;
