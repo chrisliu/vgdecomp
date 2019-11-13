eff17ba8564a673f50e8b45c91a0c74b1e3b13f4
Ryan <ryan.h.nishimoto@gmail.com>
2019-11-13 12:31:13 -0800

    updated makefile, added more tests

--- test/PathConnectedTest.cpp
+++ test/PathConnectedTest.cpp
@@ -12,14 +12,24 @@ int main(int argv, char* argc[]){
     files.push_back("test/path_connected_tests/chain_reversals.json");
     files.push_back("test/path_connected_tests/nested_bubbles.json");
     files.push_back("test/path_connected_tests/two_bubbles_adjacent.json");
-
+    files.push_back("test/path_connected_tests/trivial_cycle.json");
+    files.push_back("test/bundle_test_graphs/30_two_strongly_adjacent_bundles.json");
+    files.push_back("test/bundle_test_graphs/40_two_strongly_adjacent_bundles_flipped.json");
+    files.push_back("test/bundle_test_graphs/31_two_weakly_adjacent_bundles.json");
+    files.push_back("test/bundle_test_graphs/41_two_weakly_adjacent_bundles_flipped.json");
+    files.push_back("test/bundle_test_graphs/42_triple_cyclic_bundles.json");
+    files.push_back("test/bundle_test_graphs/15_acyclic_useless_bundle.json");
+    files.push_back("test/bundle_test_graphs/04_complex_cyclic_useless_bundle.json");
+    
+    
     BidirectedGraphBuilder builder;
     vector<string>::iterator iter = files.begin();
     while(iter!=files.end()){
         BidirectedGraph graph = builder.build_graph(*iter);
-        unordered_map<const handle_t, vector<handle_t> > map = find_path_connected(&graph);
         cout << *iter << "\n";
+        unordered_map<const handle_t, vector<handle_t> > map = find_path_connected(&graph);
         print_path_connected(&graph, map);
+        iter++;
     }
 
 }
