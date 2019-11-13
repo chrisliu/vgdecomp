e06feb0c26de9903c3a466543b029f20c07f47c4
Ryan <ryan.h.nishimoto@gmail.com>
2019-11-13 11:07:01 -0800

    added some basic test cases

--- src/algorithms/find_path_connected.cpp
+++ src/algorithms/find_path_connected.cpp
@@ -5,20 +5,25 @@
 std::unordered_map<const handle_t, vector<handle_t> > find_path_connected(const HandleGraph* g){
     unordered_map<const handle_t, vector<handle_t> > vectors = unordered_map<const handle_t, vector<handle_t> >();
     g->for_each_handle([&](const handle_t& parent_handle){
+        cout << "iterating through node" << g->get_id(parent_handle) << '\n';
         unordered_map<const handle_t,bool> visitmap = unordered_map<const handle_t, bool>();
         unordered_set<handle_t> visited_handles = unordered_set<handle_t>();
         queue<const handle_t> queue_left;
         queue<const handle_t> queue_right;
         
         g->follow_edges(parent_handle, true, [&](const handle_t& handle){
-            queue_right.push(handle);
+            visited_handles.insert(const_cast<handle_t&>(handle));
+            visitmap[handle] = true;
+            queue_left.push(handle);
             return true;
         });
         g->follow_edges(parent_handle, false, [&](const handle_t& handle){
+            visited_handles.insert(const_cast<handle_t&>(handle));
+            visitmap[handle] = true;
             queue_right.push(handle);
             return true;
         });
-        while(queue_left.size()!=0 && queue_right.size()!=0){
+        while(queue_left.size()!=0){
             const handle_t currnode = queue_left.front();
             queue_left.pop();
             g->follow_edges(currnode, true, [&](const handle_t& handle){
@@ -32,7 +37,7 @@ std::unordered_map<const handle_t, vector<handle_t> > find_path_connected(const 
         }
         while(queue_right.size()!=0){
             const handle_t currnode = queue_right.front();
-            queue_left.pop();
+            queue_right.pop();
             g->follow_edges(currnode, false, [&](const handle_t& handle){
                 if(visitmap.find(handle)==visitmap.end()){
                     visited_handles.insert(const_cast<handle_t&>(handle));
@@ -42,41 +47,11 @@ std::unordered_map<const handle_t, vector<handle_t> > find_path_connected(const 
                 return true;
             });
         }
-        /*
-        queue.push(handle);
-        colormap[handle] = 'g';
-        while(queue.size()!=0){
-            const handle_t currnode = queue.front();
-            queue.pop();
-            follow_edges_impl(currnode, true, [&](const handle_t& handle){
-                if(colormap.find(handle)!=colormap.end() && colormap[handle]=='b'){
-                    //push entire reachable list if black
-                    //if already discovered by someone else
-                    //
-                    //return true;
-                }
-                else{
-                    queue.push(handle);
-                    component.push_back(&handle);
-                    colormap[handle] = 'g';
-                    return true;
-                } 
-            });
-            follow_edges_impl(currnode, false, [&](const handle_t& handle){
-
-            });
-        }
-        colormap[handle] = 'b';
-        vector<const handle_t*>::iterator iter= component.begin();
-        while(iter!=component.end()){
-            reachable_nodes.emplace(get_id(**iter), component);
-            ++iter;
-        }
-        return true;*/
         unordered_set<handle_t>::iterator iter = visited_handles.begin();
         vectors[parent_handle] = vector<handle_t>();
         while(iter!=visited_handles.end()){
             vectors[parent_handle].push_back(*iter);
+            iter++;
         }
         return true;
 
@@ -97,8 +72,10 @@ void print_path_connected(const HandleGraph *g, unordered_map<const handle_t, ve
         veciter = iter->second.begin();
         while(veciter!=iter->second.end()){
             cout << g->get_id(*veciter) << ", ";
+            veciter++;
         }
         cout << "\n";
+        iter++;
         
     }
 }
--- test/PathConnectedTest.cpp
+++ test/PathConnectedTest.cpp
@@ -3,10 +3,23 @@
 #include "../src/algorithms/find_path_connected.hpp"
 
 using namespace std;
-
+vector<string> files;
 int main(int argv, char* argc[]){
+    files.push_back("test/test.json");
+    files.push_back("test/path_connected_tests/bubble.json");
+    files.push_back("test/path_connected_tests/bubble_reversals.json");
+    files.push_back("test/path_connected_tests/chain.json");
+    files.push_back("test/path_connected_tests/chain_reversals.json");
+    files.push_back("test/path_connected_tests/nested_bubbles.json");
+    files.push_back("test/path_connected_tests/two_bubbles_adjacent.json");
+
     BidirectedGraphBuilder builder;
-    BidirectedGraph graph = builder.build_graph("test/test.json");
-    unordered_map<const handle_t, vector<handle_t> > map = find_path_connected(&graph);
-    print_path_connected(&graph, map);
+    vector<string>::iterator iter = files.begin();
+    while(iter!=files.end()){
+        BidirectedGraph graph = builder.build_graph(*iter);
+        unordered_map<const handle_t, vector<handle_t> > map = find_path_connected(&graph);
+        cout << *iter << "\n";
+        print_path_connected(&graph, map);
+    }
+
 }
