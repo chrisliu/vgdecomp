52b023cbd767f007ed2cfbfd3b7b6ee2311fd540
Chris Liu <chrisliu1999@gmail.com>
2019-12-21 16:19:50 -0800

    Updated code to reflect refactored find balanced bundles algorithm and improved output readability.

--- test/test_bundle/BundleTest.cpp
+++ test/test_bundle/BundleTest.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 #include "../../src/BidirectedGraph.hpp"
 #include "../../src/BidirectedGraphBuilder.hpp"
@@ -7,15 +9,22 @@
 
 using namespace std;
 
-void print_bundle(BidirectedGraph* g, Bundle& bundle) {
+std::string node_to_str(const handle_t& handle, const HandleGraph& g) {
+    stringstream ss;
+    ss << "Node " << g.get_id(handle);
+    if (g.get_is_reverse(handle)) ss << " reversed";
+    return ss.str();
+}
+
+void print_bundle(BidirectedGraph& g, Bundle& bundle) {
     cout << "Left" << endl;
     bundle.get_bundleside(true).traverse_bundle([&](const handle_t& l_handle) {
-        cout << g->get_id(l_handle) << ((g->get_is_reverse(l_handle)) ? '-' : '+') << endl;
+        cout << node_to_str(l_handle, g) << endl;
         return true;
     });
     cout << "Right" << endl;
     bundle.get_bundleside(false).traverse_bundle([&](const handle_t& r_handle) {
-        cout << g->get_id(r_handle) << ((g->get_is_reverse(r_handle)) ? '-' : '+') << endl;
+        cout << node_to_str(r_handle, g) << endl;
         return true;
     }); 
     cout << "Is a trivial bundle:  " << ((bundle.is_trivial()) ? "true" : "false") << endl;
@@ -31,9 +40,9 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; i++) {
         cout << i << "\t" << argv[i] << endl;
         BidirectedGraph g = BidirectedGraphBuilder::build_graph(argv[i]);
-        auto bundles = find_balanced_bundles(&g);
+        auto bundles = find_balanced_bundles(g);
         for (auto bundle : bundles) {
-            print_bundle(&g, bundle);
+            print_bundle(g, bundle);
         }
         cout << "Nodes: ";
         g.for_each_handle([&](const handle_t& handle) {
