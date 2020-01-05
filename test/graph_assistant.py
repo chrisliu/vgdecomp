a46f164543906996db3b3d7fd017fa7ee7f25628
Chris Liu <chrisliu1999@gmail.com>
2020-01-05 00:54:42 -0800

    Rewrote to use vg's json format.

--- test/graph_assistant.py
+++ test/graph_assistant.py
@@ -1,222 +1,222 @@
+#!/usr/bin/env python3
+import argparse
 import json
-
-exit_codes = ['q', 'Q']
+import re
+from typing import Callable, Optional
 
 def main():
-    print_help()
+    parser = setup_parser()
+    parsed = parser.parse_args()
+    
+    process_args(parsed)
 
-    # Ask for filename
-    filename = get_filename() 
+def setup_parser():
+    parser = argparse.ArgumentParser(
+        prog = 'graph_assistant_dev',
+        description = 'A tool to generate custom graphs in vg\'s json format.'
+    )
+    parser.add_argument('--tool-help', 
+        action = 'store_true',
+        help = 'explains format for adding nodes and edges (and bundles)'
+    )
+    parser.add_argument('-b', '--bundles',
+        action = 'store_true',
+        help = 'enable bundle declaration'
+    )
+    parser.add_argument('-d', '--default-nodes',
+        action = 'store_true',
+        help = 'don\'t ask for node sequences'
+    )
+    parser.add_argument('-c', '--vg-compliant',
+        action = 'store_true',
+        help = 'output another vg compliant version'
+    )
+    return parser
 
-    # Get comment
-    comment = input("Comment: ")
+def process_args(parsed: argparse.Namespace):
+    ## Tool help has highest precedence and exits when done
+    if parsed.tool_help:
+        display_tool_help()
+        return
 
-    # Add edges
-    print("Add edges!")
-    edges = list()
-    while True:
-        edge = add_edge()
-        if edge is None:
-            break
-        edges.append(edge)
+    ## Graph dictionary
+    graph = dict()
+    
+    ## If not default nodes, ask to initialize node sequences
+    if not parsed.default_nodes:
+        prompt_nodes(graph)
+    
+    prompt_edges(graph)
 
-    # Get nodes
-    l_nodes = {e.id1 for e in edges}
-    r_nodes = {e.id2 for e in edges}
-    nodes = list(l_nodes | r_nodes)
+    ## If bundles are needed, prompt
+    if parsed.bundles:
+        prompt_bundles(graph)
 
-    # Get bundles    
-    print("Add bundles!")
-    bundles = list()
-    while True:
-        bundle = add_bundle()
-        if bundle is None:
-            break
-        bundles.append(bundle)
+    ## If default nodes was asked, initialize nodes
+    if parsed.default_nodes:
+        init_nodes(graph)
 
-    graph = {
-        'comment': comment,
-        'graph': {
-            'edges': [
-                edge.dump() for edge in edges
-            ],
-            'nodes': [
-                {
-                    'id': node_id
-                } for node_id in nodes
-            ]
-        },
-        'bundles': [
-            bundle.dump() for bundle in bundles
-        ]
-    }
+    ## Prompt for a filename
+    filename = prompt_name()
 
-    with open(filename, 'w') as out:
+    ## Prompt for a description
+    prompt_desc(graph)
+
+    ## Reorder graph (aesthetic)
+    graph = reorder(graph)
+
+    ## Serialize non-vg compliant version
+    serialize(graph, filename)
+
+    ## If vg compliant version is asked, serialize one
+    if parsed.vg_compliant:
+        serialize_vg(graph, filename)
+
+def display_tool_help():
+    tool_help_msg = """This is the tool help message
+
+    """
+    print(tool_help_msg)
+
+def prompt_nodes(graph: dict):
+    graph['node'] = list()
+
+    node_pattern = re.compile('\d+')
+    sequence_pattern = re.compile('[ACGTacgt]*')
+
+    def valid_node(response: str) -> bool:
+        return node_pattern.fullmatch(response) is not None
+
+    def valid_sequence(response: str) -> bool:
+        return sequence_pattern.fullmatch(response) is not None
+
+    def add_node() -> bool:
+        ## Prompt for id and sequence but allow for early exits
+        node_id = proper_input("| Node: ", valid_node, def_exit)
+        if node_id is None: return False
+        sequence = proper_input("| Sequence: ", valid_sequence, def_exit)
+        if sequence is None: return False
+
+        ## Add node
+        graph['node'].append(
+            {
+                'id': int(node_id),
+                'sequence': sequence.upper()
+            }
+        )
+
+        return True
+    
+    ## Keep adding nodes until exit
+    print("Nodes:")
+    while add_node(): pass
+
+def prompt_edges(graph: dict):
+    graph['edge'] = list()
+
+    node_side_pattern = re.compile('(\d+)([lLrR]?)')
+
+    def valid_node_side(response: str) -> bool:
+        return node_side_pattern.fullmatch(response) is not None
+    
+    def add_edge() -> bool:
+        ## Prompt for node sides but allow for early exits
+        node1 = proper_input("| Node 1: ", valid_node_side, def_exit)
+        if node1 is None: return False
+        node2 = proper_input("| Node 2: ", valid_node_side, def_exit)
+        if node2 is None: return False
+
+        ## Get edge information
+        node1_match = node_side_pattern.fullmatch(node1)
+        node2_match = node_side_pattern.fullmatch(node2)
+        
+        node1_id = int(node1_match.group(1))
+        node2_id = int(node2_match.group(1))
+        from_left = False if node1_match.group(2) in ['', 'r'] else True
+        to_right = False if node2_match.group(2) in ['', 'l'] else True
+
+        edge = {'from': node1_id, 'to': node2_id}
+        if from_left: edge['from_start'] = True
+        if to_right: edge['to_end'] = True
+        graph['edge'].append(edge)
+
+        return True
+
+    ## Keep adding edges until exit
+    print("Edges:")
+    while add_edge(): pass
+
+def prompt_bundles(graph: dict):
+    graph['bundle'] = list()
+    print("(debug) Prompting bundles")
+
+def init_nodes(graph: dict):
+    """Gets all nodes in defined edges and assigns an empty sequence."""
+    ## Get all nodes defined in 'edge'
+    nodes = set()
+    for edge in graph['edge']:
+        nodes.add(edge['from'])
+        nodes.add(edge['to'])    
+    nodes = sorted(nodes)
+
+    graph['node'] = [{'id': node_id, 'sequence': ''} for node_id in nodes]
+
+def prompt_name() -> str:
+    """Prompts and returns a formatted filename."""
+    filename = proper_input("Filename: ", lambda s: s[-5:] != '.json')
+    filename = filename.replace(' ', '_')
+    filename = filename.strip('_')
+    return filename
+
+def prompt_desc(graph: dict):
+    """Prompts and adds description to graph dictionary."""
+    description = proper_input("Description: ")
+    graph['description'] = description
+
+def reorder(graph: dict) -> dict:
+    """Reorder dictionary so graph info can be seen first."""
+    first_keys = ['description']
+    new_graph = dict()
+
+    for key in first_keys: 
+        new_graph[key] = graph[key]
+    
+    # Put the rest in 
+    for k, v in graph.items():
+        if k not in first_keys:
+            new_graph[k] = v
+    
+    return new_graph
+
+def serialize(graph: dict, filename: str):
+    """Serializes a custom vg inspired json format."""
+    with open('{}.json'.format(filename), 'w') as out:
         json.dump(graph, out, indent = 4)
 
-    print(f"Outputted to {filename}")
+def serialize_vg(graph: dict, filename: str):
+    """Serializes a vg compliant json format."""
+    vg_graph = dict()
+    vg_graph['node'] = graph['node']
+    vg_graph['edge'] = graph['edge']
+    with open('{}_vg.json'.format(filename), 'w') as out:
+        json.dump(vg_graph, out, indent = 4)
 
-def print_help():
-    instructions = """Graph Assistant Instructions
-    1. Edge declarations phase
-       - Prompts for the first and second nodes in the edge.
-         Ex.
-            Node 1: <Node1 str>
-            Node 2: <Node2 str>
-         Node string format: \d+[lr]?
-         l and r denotes the left and right side of a node respectively.
-         When done, use 'q' to exit the edge declaration phase.
-    2. Bundle declaration phase
-       - Prompts for a bundle.
-         Ex.
-            Finish bundles? <Finished str>
-         Type 'qq' to indicate there are no more bundles and exit tool.
-         Type <enter> to declare a new bundle.
-       - Prompts for the left side of a bundle.
-         Ex.
-            Left side node: <Node1 str>
-            > <Node2 str>
-         Node string format: \d+r?
-         r is used to indicate that the node will be reversed.
-         Type 'q' when all nodes on the left side have been declared.
-       - Prompts for the right side of a bundle.
-         The steps are the same as the left side.
-    3. Custom json format will be outputted to <difficulty><id>_<name>.json
-    """
-    print(instructions)
-
-def _keep_prompting(prompt, condition):
+def proper_input(prompt: str, 
+    check_fn: Optional[Callable[[str], bool]] = lambda _ : True, 
+    exit_fn: Optional[Callable[[str], bool]] = lambda _ : False) -> str:
+    """Keeps prompting until condition or exit is met."""
     while True:
-        input_str = input(prompt)
-        if condition(input_str):
-            return 
+        response = input(prompt)
+        ## If exit, return None
+        if exit_fn(response):
+            return None
+        
+        ## If valid response, return response
+        if check_fn(response):
+            return response
 
-def get_filename():
-    """Returns a formatted filename."""
-    difficulty = input("Test graph difficulty: ")
-    count = input("Test graph count: ")
-    name = input("Test graph name: ")
-
-    return f"{difficulty}{count}_{name.lower().replace(' ', '_')}.json" 
-    
-def add_edge():
-    """Returns a parsed edge or None
-    
-    If the exit code is entered, returns None.
-
-    Returns:
-        An Edge object
-    """
-
-    edge = Edge()
-
-    def parse_input(node_str, is_first = True):
-        """Returns a tuple of (node_id, is_reverse)."""
-        return int(node_str[:-1]), node_str[-1] == ('l' if is_first else 'r')
-    
-    def is_exit(input_str):
-        return input_str in exit_codes
-
-    def get_node(prompt, is_first = True):
-        """Processes node or exits.
-
-        Returns:
-            A boolean indicating if an exit command was given.
-        """
-
-        while True:
-            input_str = input(prompt)
-            if is_exit(input_str):
-                return True
-            if input_str[-1] not in "lr":
-                input_str += 'r' if is_first else 'l'
-            try:
-                if is_first:
-                    edge.id1, edge.from_left = parse_input(input_str) 
-                else:
-                    edge.id2, edge.to_right = parse_input(input_str, False)
-                return False
-            except:
-                continue
-
-    if get_node("Node 1: ", is_first = True):
-        return None
-    if get_node("Node 2: ", is_first = False):
-        return None
-    
-    return edge
-
-def add_bundle():
-    if input("Finish bundles? ") == 'qq':
-        return None
-
-    def parse_input(node_str):
-        """Returns a tuple of (node_id, is_reverse)."""
-        return int(node_str[:-1]), node_str[-1] == 'r'
-
-    def is_exit(input_str):
-        return input_str in exit_codes
-
-    def get_node(prompt):
-        while True:
-            input_str = input(prompt)
-            if is_exit(input_str):
-                return None
-            if input_str[-1] not in "fr":
-                input_str += 'f'
-            try:
-                return parse_input(input_str)
-            except:
-                continue
-
-    # Get left side
-    bundle = Bundle()
-
-    while True:
-        l_node = get_node("Left side node: " if not len(bundle.left_nodes) else "> ")
-        if l_node is None:
-            break
-        bundle.left_nodes.append(l_node)
-    
-    while True:
-        r_node = get_node("Right side node: " if not len(bundle.right_nodes) else "> ")
-        if r_node is None:
-            break
-        bundle.right_nodes.append(r_node)
-
-    return bundle
-
-class Edge(object):
-    def __init__(self):
-        self.id1 = -1;
-        self.id2 = -1;
-        self.from_left = False;
-        self.to_right = False;
-    
-    def dump(self):
-        """Returns edge as a compliant edge object."""
-        return {
-            'id1': self.id1,
-            'id2': self.id2,
-            'from_left': self.from_left,
-            'to_right': self.to_right
-        }
-
-class Bundle(object):
-    def __init__(self):
-        self.left_nodes = list()
-        self.right_nodes = list()
-    
-    def dump(self):
-        """Returns bundle as a compliant bundle object."""
-        return {
-            'left': [
-                {'id': node_id, 'is_reverse': is_reverse} for (node_id, is_reverse) in self.left_nodes
-            ],
-            'right': [
-                {'id': node_id, 'is_reverse': is_reverse} for (node_id, is_reverse) in self.right_nodes
-            ]
-        }
+def def_exit(response: str) -> bool:
+    return response in ['q', 'Q']
 
 if __name__ == '__main__':
     main()
