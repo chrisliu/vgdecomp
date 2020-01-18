848a21dcd13b78264ac37f3bd4024dcffe45d0ea
Chris Liu <chrisliu1999@gmail.com>
2020-01-18 00:43:18 -0800

    Deprecated BidirectedGraphBuilder.

--- test/graphbuilder/GraphBuilderTest.cpp
+++
@@ -1,8 +0,0 @@
-#include <cstdlib>
-#include <string>
-#include "../../src/BidirectedGraphBuilder.hpp"
-
-int main(int argc, char* argv[]) {
-    BidirectedGraphBuilder builder;
-    BidirectedGraph graph = builder.build_graph(argv[1]);
-}
