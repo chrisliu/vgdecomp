e0d0ab5f5f69605bc2d32ef43b3c9262695c0d10
Chris Liu <chrisliu1999@gmail.com>
2020-01-21 18:27:30 -0800

    Removed caching (set -> vector) in order to reduce complexity of bundle.

--- src/algorithms/bundle.cpp
+++ src/algorithms/bundle.cpp
@@ -1,15 +1,9 @@
 #include "bundle.hpp"
 #include <algorithm>
-#include <iterator>
-#include <stdexcept>
-#include <unordered_map>
 #include "../../deps/handlegraph/util.hpp"
 
+using namespace std;
 using namespace handlegraph;
-
-/***********************************************
- * Custom structures implementation
- ***********************************************/
 
 /// Counter structure
 struct Count {
@@ -18,64 +12,21 @@ struct Count {
     size_t count = 0;
 };
 
-/***********************************************
- * BundleSide implementation
- ***********************************************/
-
-bool BundleSide::add_node(const handle_t& node) {
-    if (is_bundle_freed) {
-        return true;
-    }
-
-    return bundle_set.insert(node).second;
+bool BundleSide::add_node(const handle_t& handle) {
+    return nodes.insert(handle).second;
 }
 
-void BundleSide::cache(HandleGraph& g) {
-    for (const handle_t& node : bundle_set) {
-        bundle_vector.push_back(node);
-        bundle_vector_reversed.push_back(g.get_handle(g.get_id(node), !g.get_is_reverse(node)));
-    }
-
-    // Should prompt C++ to clear the memory
-    bundle_set = std::unordered_set<handle_t>();
-    // Else this is the method I came up with
-    /*
-    bundle_set.clear();
-    bundle_set.rehash(1);
-    */
-
-    /// Sort for set intersection
-    std::sort(bundle_vector.begin(), bundle_vector.end(), 
-        [](handle_t& h1, handle_t& h2) {return as_integer(h1) < as_integer(h2);});
-    std::sort(bundle_vector_reversed.begin(), bundle_vector_reversed.end(),
-        [](handle_t& h1, handle_t& h2) {return as_integer(h1) < as_integer(h2);});
-
-    is_bundle_freed = true;
-}
-
-void BundleSide::traverse_bundle(const std::function<void(const handle_t&)>& iteratee) {    
-    if (is_bundle_freed) {
-        for (const handle_t& node : bundle_vector) {
-            iteratee(node);
-        }
-    } else {
-        for (const handle_t& node : bundle_set) {
-            iteratee(node);
-        }
+void BundleSide::update(const HandleGraph& g) {
+    nodes_flipped.clear();
+    for (auto& node : nodes) {
+        nodes_flipped.insert(g.flip(node));
     }
 }
 
-int BundleSide::size() {
-    if (is_bundle_freed) {
-        return bundle_vector.size();
-    }
-    return bundle_set.size();
-}
-
-Adjacency get_adjacency_type(const bundle_vector_t side1, const bundle_vector_t side2) {
+Adjacency _get_adjacency_type(const unordered_set<handle_t> side1, const unordered_set<handle_t> side2) {
     Count c;
-    set_intersection(side1.begin(), side1.end(), side2.begin(), side2.end(), std::back_insert_iterator(c), 
-        [](const handle_t h1, const handle_t h2) -> bool {
+    set_intersection(side1.begin(), side1.end(), side2.begin(), side2.end(),
+        back_insert_iterator(c), [](const handle_t& h1, const handle_t& h2) {
             return as_integer(h1) < as_integer(h2);
         }
     );
@@ -90,65 +41,18 @@ Adjacency get_adjacency_type(const bundle_vector_t side1, const bundle_vector_t 
     return Adjacency::None;
 }
 
-Adjacency BundleSide::get_adjacency(const BundleSide& other) const {
-    if (!this->is_bundle_freed || !other.is_bundle_freed) {
-        throw std::invalid_argument("Invalid BundleSide in adjacency");
-    }
-
+Adjacency BundleSide::get_adjacency_type(const BundleSide& other) const {
     Adjacency res;
-    if ((res = get_adjacency_type(this->bundle_vector, other.bundle_vector)) != Adjacency::None ||
-        (res = get_adjacency_type(this->bundle_vector, other.bundle_vector_reversed)) != Adjacency::None ||
-        (res = get_adjacency_type(this->bundle_vector_reversed, other.bundle_vector)) != Adjacency::None ||
-        (res = get_adjacency_type(this->bundle_vector_reversed, other.bundle_vector_reversed)) != Adjacency::None
-    ) {
+    if ((res = _get_adjacency_type(nodes, other.nodes)) != Adjacency::None ||
+        (res = _get_adjacency_type(nodes, other.nodes_flipped)) != Adjacency::None ||
+        (res = _get_adjacency_type(nodes_flipped, other.nodes)) != Adjacency::None ||
+        (res = _get_adjacency_type(nodes_flipped, other.nodes_flipped)) != Adjacency::None) {
         return res;
     }
     return Adjacency::None;
 }
 
-/***********************************************
- * Bundle implementation
- ***********************************************/
-bool Bundle::add_node(const handle_t& node, bool is_left) {
-    if (is_left) {
-        return internal_bundle.first.add_node(node);
-    } else {
-        return internal_bundle.second.add_node(node);
-    }
-}
-
-void Bundle::add_init_node(const handle_t& node, bool is_left) {
-    add_node(node, is_left);
-}
-
-int Bundle::get_bundleside_size(bool is_left) {
-    return get_bundleside(is_left).size();
-}
-
-BundleSide Bundle::get_bundleside(bool is_left) {
-    if (is_left) {
-        return internal_bundle.first;
-    }
-    return internal_bundle.second;
-}
-
-void Bundle::freeze(HandleGraph& g) {
-    internal_bundle.first.cache(g);
-    internal_bundle.second.cache(g);
-}
-
-bool Bundle::is_trivial() {
-    return is_bundle_trivial; 
-}
-
-void Bundle::set_trivial(bool is_bundle_trivial_) {
-    this->is_bundle_trivial = is_bundle_trivial_;
-}
-
-bool Bundle::has_reversed_node() {
-    return has_reversed;
-}
-
-void Bundle::set_has_reversed_node(bool has_reversed_) {
-    this->has_reversed = has_reversed_;
+void Bundle::update_bundlesides(const HandleGraph& g) {
+    left.update(g);
+    right.update(g);
 }
--- src/algorithms/bundle.hpp
+++ src/algorithms/bundle.hpp
@@ -1,16 +1,11 @@
-#ifndef VG_BUNDLE_HPP_INCLUDED
-#define VG_BUNDLE_HPP_INCLUDED
+#ifndef VG_ALGORITHMS_BUNDLE_HPP_INCLUDED
+#define VG_ALGORITHMS_BUNDLE_HPP_INCLUDED
 
-#include <utility>
 #include <unordered_set>
 #include <vector>
 
-#include "../../deps/handlegraph/handle_graph.hpp"
-#include "../../deps/handlegraph/iteratee.hpp"
 #include "../../deps/handlegraph/types.hpp"
-
-typedef std::unordered_set<handlegraph::handle_t> bundle_set_t;
-typedef std::vector<handlegraph::handle_t> bundle_vector_t;
+#include "../../deps/handlegraph/handle_graph.hpp"
 
 enum Adjacency {
     None,
@@ -18,130 +13,72 @@ enum Adjacency {
     Strong
 };
 
+/// Glorified wrapper for std::unordered_set<handle_t>
 class BundleSide {
     private:
-        bundle_set_t    bundle_set;
-        bundle_vector_t bundle_vector;
-        bundle_vector_t bundle_vector_reversed;
-        bool            is_bundle_freed;
-
+        std::unordered_set<handlegraph::handle_t> nodes;
+        std::unordered_set<handlegraph::handle_t> nodes_flipped;
     public:
-        /// Adds a node handle to this bundle side. 
-        /// If this bundle side hasn't been cached, the new
-        /// node handle will be stored in bundle_set. 
-        /// A bool will be returned to indicate whether or
-        /// not the node handle already exists.
-        /// If the bundle side has been cached, true will
-        /// be returned since the nodes in the unordered
-        /// set would've been freed.
-        /// Parameters:
-        ///   1. node: the handle for the node to be added
-        /// Returns:
-        ///   1. true: if the node handle already exists
-        ///            or if this bundle side has been
-        ///            cached
-        ///   2. false: if the node handle doesn't exist
-        bool add_node(const handlegraph::handle_t& node);
+        /// Returns true if successful addition, false if otherwise
+        bool add_node(const handlegraph::handle_t& handle);
 
-        /// Cache bundle side so future iterator operations
-        /// will be faster. Converts an unordered set into
-        /// a vector. 
-        /// This medium article discusses the performance
-        /// differences between std::vector and std::unorderd_set
-        /// https://medium.com/@gx578007/searching-vector-set-and-unordered-set-6649d1aa7752
-        /// Unordered sets are used in the add node process 
-        /// for improved adding performance
-        /// Vectors are used in the iterating process since 
-        /// it's more memory efficient and faster.
-        /// bundle_set memory will be freed when this function
-        /// is called to conserve memory space.
-        void cache(handlegraph::HandleGraph& g);
+        void add_init_node(const handlegraph::handle_t& handle) {
+            add_node(handle);
+        }
 
-        /// Iterate through the bundle side with an iterator
-        /// Parameters:
-        ///   1. iteratee: lambda function that takes a
-        ///                const handle_t& as its input
-        /// Returns:
-        ///   1. true: if the bundle side's nodes have been
-        ///            traversed successfully without an
-        ///            an early exit triggered by the lambda 
-        ///            function
-        ///   2. false: if the lambda function triggers an
-        ///             early exit
-        /// *Note*
-        /// Originally I wanted to return a generic iterator
-        /// for either bundle_set or bundle_vector depending
-        /// on whether or not it's been cached already. However,
-        /// I couldn't figure out if it's possible in C++
-        void traverse_bundle(const std::function<void(const handlegraph::handle_t&)>& iteratee);
+        size_t size() const { return nodes.size(); }
+        
+        std::unordered_set<handlegraph::handle_t>::iterator begin() {
+            return nodes.begin();
+        }
 
-        /// See: https://lemire.me/blog/2017/01/27/how-expensive-are-the-union-and-intersection-of-two-unordered_set-in-c/
-        Adjacency get_adjacency(const BundleSide& other) const;
+        std::unordered_set<handlegraph::handle_t>::iterator end() {
+            return nodes.end();
+        }
 
-        int size();
+        std::unordered_set<handlegraph::handle_t>::const_iterator cbegin() const {
+            return nodes.cbegin();
+        }
+
+        std::unordered_set<handlegraph::handle_t>::const_iterator cend() const {
+            return nodes.cend();
+        }
+
+        void update(const handlegraph::HandleGraph& g);
+
+        Adjacency get_adjacency_type(const BundleSide& other) const;
+
+        std::vector<handlegraph::handle_t> get_nodes() { return std::vector<handlegraph::handle_t>(nodes.begin(), nodes.end()); }
 };
 
-// *Note*
-// Bundle will not check if a node that's added to one side
-// already exists on the other side.
-// This has not been implemented yet and will need to be to
-// double check self cycles. Self cycles could break the 
-// algorithm.
+/// Glorified wrapper for std::pair<BundleSide, BundleSide>
 class Bundle {
     private:
-        typedef std::pair<BundleSide, BundleSide> bundle_t;
-        
-        bundle_t internal_bundle;
-    
+        BundleSide left;
+        BundleSide right;
         bool is_bundle_trivial;
-        bool has_reversed;     
+        bool has_reversed;
+
     public:
-        /// Add a node handle to the bundle with the side
-        /// indicated by is_left. Returns whether or not
-        /// the insertion is a success. If it isn't a success
-        /// that mean this structure is not a bundle and all
-        /// future attempt to add a node will return the same
-        /// value.
-        /// Parameters:
-        ///   1. node: the handle for the node to be added
-        ///   2. is_left: the side of a bundle the node
-        ///               is going to be added to. 
-        ///               true if it's on the left
-        ///               false if it's on the right
-        /// Returns:
-        ///   1. true: if the node is successfully added to
-        ///            the side (not a duplicate)
-        ///   2. false: if a node is unsuccessfully added
-        ///             to the side (is a duplicate) or
-        ///             some previous add action resulted
-        ///             in this bundle to be invalid
-        bool add_node(const handlegraph::handle_t& node, bool is_left = true);
+        BundleSide& get_bundleside(bool is_left) {
+            return is_left ? left : right;
+        }
+        
+        BundleSide& get_left() {
+            return left;
+        }
 
-        /// Add a node handle to the bundle with the side
-        /// indiacted by is_left. This operation will not
-        /// care about duplicate values and should only
-        /// be used to initialize a side a bundle.
-        /// Parameters:
-        ///   1. node: the handle for the node to be added
-        ///   2. is_left: the side of a bundle the node
-        ///               is going to be added to.
-        ///               true if it's on the left
-        ///               false if it's on the right
-        void add_init_node(const handlegraph::handle_t& node, bool is_left = true);
+        BundleSide& get_right() {
+            return right;
+        }
 
-        int get_bundleside_size(bool is_left);
+        bool is_trivial() { return is_bundle_trivial; }
+        void set_trivial(bool is_bundle_trivial_) { is_bundle_trivial = is_bundle_trivial_;}
 
-        /// Access the Bundleside variables for each side
-        /// Left side bundle access
-        BundleSide get_bundleside(bool is_left);
+        bool has_reversed_node() { return has_reversed; }
+        void set_has_reversed_node(bool has_reversed_) { has_reversed = has_reversed_; }
 
-        void freeze(handlegraph::HandleGraph& g);
-
-        /// Accessor functions
-        bool is_trivial();
-        void set_trivial(bool is_bundle_trivial_);
-
-        bool has_reversed_node();
-        void set_has_reversed_node(bool has_reversed_);
+        void update_bundlesides(const handlegraph::HandleGraph& g);
 };
-#endif /* VG_BUNDLE_HPP_INCLUDED */
+
+#endif /* VG_ALGORITHMS_BUNDLE_HPP_INCLUDED */
