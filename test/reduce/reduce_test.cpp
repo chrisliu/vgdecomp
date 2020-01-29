3a8d32e70c5174d7b94c77f353659bd563de661d
Chris Liu <chrisliu1999@gmail.com>
2020-01-29 12:55:21 -0800

    Implemented an untested version of the decomposition of the email graph.

--- test/reduce/reduce_test.cpp
+++ test/reduce/reduce_test.cpp
@@ -7,6 +7,7 @@
 
 #include "../../src/BidirectedGraph.hpp"
 #include "../../src/algorithms/find_balanced_bundles.hpp"
+#include "../../src/algorithms/bundle.hpp"
 #include "../../src/algorithms/reduce.hpp"
 #include "../../deps/handlegraph/types.hpp"
 
@@ -15,12 +16,12 @@ using namespace handlegraph;
 
 using bundle_map_t = unordered_map<handle_t, Bundle*>;
 
-inline void mark_bundle(HandleGraph& g, Bundle& bundle, bundle_map_t& bundle_map) {
-    for (auto& handle : bundle.get_left()) {
-        bundle_map[handle] = &bundle;
+inline void mark_bundle(HandleGraph& g, Bundle* bundle, bundle_map_t& bundle_map) {
+    for (auto& handle : bundle->get_left()) {
+        bundle_map[handle] = bundle;
     }
-    for (auto& handle : bundle.get_right()) {
-        bundle_map[g.flip(handle)] = &bundle;
+    for (auto& handle : bundle->get_right()) {
+        bundle_map[g.flip(handle)] = bundle;
     }
 }
 
@@ -32,6 +33,7 @@ void perform_reduction1(DeletableHandleGraph& g, handle_t& node, bundle_map_t& b
     /// Delete node entry in bundle_map if any
     if (bundle_map.count(node)) bundle_map.erase(node);
     if (bundle_map.count(g.flip(node))) bundle_map.erase(g.flip(node));
+    /// TODO: Delete bundle and remove node entry in other bundles
     
     /// Perform reduction action 1
     reduce_degree_one_node(g, node);
@@ -42,24 +44,43 @@ void perform_reduction1(DeletableHandleGraph& g, handle_t& node, bundle_map_t& b
 }
 
 void perform_reduction2(DeletableHandleGraph& g, Bundle& bundle, bundle_map_t& bundle_map) {
+    /// Delete node entries in bundle
+    handle_t left = *bundle.get_left().begin();
+    handle_t right = *bundle.get_right().begin();
+    bundle_map.erase(left);
+    bundle_map.erase(g.flip(right));
+    if (bundle_map.count(g.flip(left))) bundle_map.erase(g.flip(left));
+    if (bundle_map.count(right)) bundle_map.erase(right);
+    /// TODO: Delete bundles
+
+    /// Perform reduction action 2
+    handle_t node = reduce_trivial_bundle(g, bundle);
+
+    /// Try to reinitialize bundles (if any)
+    auto [in_bundle1, bundle1] = find_balanced_bundle(g.flip(node), g);
+    if (in_bundle1) { mark_bundle(g, bundle1, bundle_map); }
+    auto [in_bundle2, bundle2] = find_balanced_bundle(node, g);
+    if (in_bundle2) { mark_bundle(g, bundle2, bundle_map); }
+}
+
+void perform_reduction3(DeletableHandleGraph& g, Bundle& bundle, bundle_map_t& bundle_map) {
     /// Remove references of the left of L nodes and right of R nodes (must be 
     /// strongly adjacent to other bundles or this reduction action won't be
     /// performed)
-    cout << "Erasing" << endl;
     for (auto& handle : bundle.get_left()) { 
-        if (bundle_map.count(handle)) bundle_map.erase(handle); 
+        bundle_map.erase(handle);
+        if (bundle_map.count(g.flip(handle))) bundle_map.erase(g.flip(handle));
     }
     for (auto& handle : bundle.get_right()) {
-        if (bundle_map.count(g.flip(handle))) bundle_map.erase(g.flip(handle)); 
+        bundle_map.erase(g.flip(handle)); 
+        if (bundle_map.count(handle)) bundle_map.erase(handle);
     }
-    cout << "Done erasing" << endl;
+    /// TODO: Delete bundle
 
     /// Reduce bundle
-    Bundle reduced_bundle = *reduce_bundle(g, bundle);
-    cout << "Getting nodes" << endl;
-    handle_t node1 = *reduced_bundle.get_left().begin();
-    handle_t node2 = *reduced_bundle.get_right().begin();
-    cout << "Got nodes" << endl;
+    Bundle* reduced_bundle = reduce_bundle(g, bundle);
+    handle_t node1 = *reduced_bundle->get_left().begin();
+    handle_t node2 = *reduced_bundle->get_right().begin();
 
     /// Add bundle created by new nodes
     mark_bundle(g, reduced_bundle, bundle_map);
@@ -71,14 +92,13 @@ void perform_reduction2(DeletableHandleGraph& g, Bundle& bundle, bundle_map_t& b
     if (in_bundle2) { mark_bundle(g, bundle2, bundle_map); }
 }
 
-void perform_reduction3() {
-
-}
-
 /// Forward declarations for verification steps
 inline void verify_init_bundles(const HandleGraph& g, const bundle_map_t& bundle_map);
 inline void verify_step1_bundles(const HandleGraph& g, const bundle_map_t& bundle_map);
 inline void verify_step2_bundles(const HandleGraph& g, const bundle_map_t& bundle_map);
+inline void verify_step3_bundles(const HandleGraph& g, const bundle_map_t& bundle_map);
+inline void verify_step4_bundles(const HandleGraph& g, const bundle_map_t& bundle_map);
+inline void verify_step5_bundles(const HandleGraph& g, const bundle_map_t& bundle_map);
 
 int main(int argc, char* argv[]) {
     /// Load graph
@@ -88,7 +108,7 @@ int main(int argc, char* argv[]) {
     g.deserialize(json_file); 
 
     /// Find and map all bundles
-    vector<Bundle> bundles = find_balanced_bundles(g);
+    vector<Bundle*> bundles = find_balanced_bundles(g);
     bundle_map_t bundle_map; /// Stores associated bundles in forward direction (go_left is false what is the bundle it enters)
     for (auto& bundle : bundles) { mark_bundle(g, bundle, bundle_map); }
 
@@ -97,6 +117,7 @@ int main(int argc, char* argv[]) {
 
     /// Perform reduction actions as described by the email
     /// Reduce <2>-<4>-<5> => <2>-<5> with R1
+    cout << "=== Step 1 ===" << endl;
     handle_t node4 = g.get_handle(4);
     perform_reduction1(g, node4, bundle_map);
     verify_step1_bundles(g, bundle_map);
@@ -106,8 +127,44 @@ int main(int argc, char* argv[]) {
     /// Reduce     /-<2>-<5>-\
     ///        <1>-     x     -<7> => <1>-<8>-<9>-<7>
     ///            \-<3>-<6>-/
-    perform_reduction2(g, *bundle_map[g.get_handle(2)], bundle_map);
+    cout << "=== Step 2 ===" << endl;
+    perform_reduction3(g, *bundle_map[g.get_handle(2)], bundle_map);
     verify_step2_bundles(g, bundle_map);
+
+    /// Reduce <1>-<8>-<9>-<7> => <1>-<9>-<7>
+    cout << "=== Step 3 ===" << endl;
+    g.follow_edges(g.get_handle(9), false, [&](const handle_t& handle) {
+        cout << "Node " << g.get_id(handle) << endl;
+    });
+    handle_t node8 = g.get_handle(8);
+    perform_reduction1(g, node8, bundle_map);
+    verify_step3_bundles(g, bundle_map);
+
+    /// Reduce <1>-<9>-<7> => <1>-<7>
+    cout << "=== Step 4 ===" << endl;
+    g.follow_edges(g.get_handle(9), false, [&](const handle_t& handle) {
+        cout << "Node " << g.get_id(handle) << endl;
+    });
+    handle_t node9 = g.get_handle(9);
+    perform_reduction1(g, node9, bundle_map);
+    verify_step4_bundles(g, bundle_map);
+
+    /// Reduce <1>-<7> => <10>
+    cout << "=== Step 5 ===" << endl;
+    perform_reduction2(g, *bundle_map[g.get_handle(1)], bundle_map);
+    verify_step5_bundles(g, bundle_map);
+
+    /// Print graph
+    cout << "=== Graph ===" << endl;
+    g.for_each_handle([&](const handle_t& handle) {
+        cout << "Node " << g.get_id(handle) << endl;
+        g.follow_edges(handle, true, [&](const handle_t& lhandle) {
+            cout << "(L) Node " << g.get_id(lhandle) << endl; 
+        });
+        g.follow_edges(handle, false, [&](const handle_t& rhandle) {
+            cout << "(R) Node " << g.get_id(rhandle) << endl;
+        });
+    });
 
     return EXIT_SUCCESS;
 }
@@ -164,5 +221,70 @@ inline void verify_step2_bundles(const HandleGraph& g, const bundle_map_t& bundl
     assert(bundle_map.count(g.get_handle(8))); // New node
     assert(bundle_map.count(g.get_handle(8, true))); // New node
     assert(bundle_map.count(g.get_handle(9))); // New node
-    assert(bundle_map.count(g.get_handle(9, true))); // New onde
+    assert(bundle_map.count(g.get_handle(9, true))); // New node
+}
+
+inline void verify_step3_bundles(const HandleGraph& g, const bundle_map_t& bundle_map) {
+    assert(bundle_map.count(g.get_handle(1)));
+    assert(!bundle_map.count(g.get_handle(1, true)));
+    assert(!bundle_map.count(g.get_handle(2))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(2, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(3))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(3, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(4))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(4, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(5))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(5, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(6))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(6, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(7)));
+    assert(bundle_map.count(g.get_handle(7, true)));
+    assert(!bundle_map.count(g.get_handle(8))); // Doesn't exist 
+    assert(!bundle_map.count(g.get_handle(8, true))); // Doesn't exist
+    assert(bundle_map.count(g.get_handle(9)));
+    assert(bundle_map.count(g.get_handle(9, true)));
+}
+
+inline void verify_step4_bundles(const HandleGraph& g, const bundle_map_t& bundle_map) {
+    assert(bundle_map.count(g.get_handle(1)));
+    assert(!bundle_map.count(g.get_handle(1, true)));
+    assert(!bundle_map.count(g.get_handle(2))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(2, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(3))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(3, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(4))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(4, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(5))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(5, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(6))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(6, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(7)));
+    assert(bundle_map.count(g.get_handle(7, true)));
+    assert(!bundle_map.count(g.get_handle(8))); // Doesn't exist 
+    assert(!bundle_map.count(g.get_handle(8, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(9))); // Doesn't exist 
+    assert(!bundle_map.count(g.get_handle(9, true))); // Doesn't exist 
+}
+
+inline void verify_step5_bundles(const HandleGraph& g, const bundle_map_t& bundle_map) {
+    assert(!bundle_map.count(g.get_handle(1))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(1, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(2))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(2, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(3))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(3, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(4))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(4, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(5))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(5, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(6))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(6, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(7))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(7, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(8))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(8, true))); // Doesn't exist
+    assert(!bundle_map.count(g.get_handle(9))); // Doesn't exist 
+    assert(!bundle_map.count(g.get_handle(9, true))); // Doesn't exist 
+    assert(!bundle_map.count(g.get_handle(10)));
+    assert(!bundle_map.count(g.get_handle(10, true)));
 }
