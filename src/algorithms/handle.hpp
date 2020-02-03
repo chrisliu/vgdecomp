6ea972b4ab81a8f7c7a0ff07a71d9a287c67591d
Chris Liu <chrisliu1999@gmail.com>
2020-02-02 17:31:58 -0800

    Updated handlegraph typedefs.

--- src/algorithms/handle.hpp
+++ src/algorithms/handle.hpp
@@ -2,11 +2,15 @@
 #define VG_ALGORITHMS_HANDLE_HPP_INCLUDED
 
 #include "../../deps/handlegraph/handle_graph.hpp"
+#include "../../deps/handlegraph/mutable_handle_graph.hpp"
+#include "../../deps/handlegraph/deletable_handle_graph.hpp"
 
 using handle_t = handlegraph::handle_t;
 using nid_t = handlegraph::nid_t;
 using edge_t = handlegraph::edge_t;
 
 using HandleGraph = handlegraph::HandleGraph;
+using MutableHandleGraph = handlegraph::MutableHandleGraph;
+using DeletableHandleGraph = handlegraph::DeletableHandleGraph;
 
 #endif /* VG_ALGORITHMS_HANDLE_HPP_INCLUDED */
